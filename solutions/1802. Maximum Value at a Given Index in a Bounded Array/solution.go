f11d3be187ef8c233c9c97e2a4f4ac2520ecd09d
Willsem <happymainer@gmail.com>
2023-06-10 22:52:18 +0300

    task(1802): golang solution

---
+++ solutions/1802. Maximum Value at a Given Index in a Bounded Array/solution.go
@@ -0,0 +1,28 @@
+func maxValue(n int, index int, maxSum int) int {
+	if n == maxSum {
+		return 1
+	}
+
+	ans := (maxSum + (index*(index+1)+(n-1-index)*(n-index))/2) / n
+
+	if ans > index && ans > n-1-index {
+		// maxSum is large enough
+		return ans
+	}
+
+	total := n
+	left, right := 0, 0
+	ans = 1
+	for total < maxSum {
+		ans++
+		if left < index {
+			left++
+		}
+		if right < n-1-index {
+			right++
+		}
+		total += left + right + 1
+	}
+
+	return ans
+}
