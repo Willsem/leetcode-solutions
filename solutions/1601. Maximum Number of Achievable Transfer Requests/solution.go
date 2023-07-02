46f7366fe5852adced33c3931369fde654dcd6b1
Willsem <happymainer@gmail.com>
2023-07-02 20:58:12 +0300

    task(1601): golang solution

---
+++ solutions/1601. Maximum Number of Achievable Transfer Requests/solution.go
@@ -0,0 +1,39 @@
+func maximumRequests(n int, requests [][]int) int {
+	popCount := func(num int) int {
+		res := 0
+		for ; num > 0; num >>= 1 {
+			res += num & 1
+		}
+		return res
+	}
+
+	answer := 0
+label:
+	for mask := 0; mask < 1<<len(requests); mask++ {
+		// fmt.Printf("%08b\n", mask)
+		indegree := make([]int, n)
+
+		bitCount := popCount(mask)
+		if bitCount <= answer {
+			continue label
+		}
+
+		for curr, pos := mask, len(requests)-1; curr > 0; curr = curr >> 1 {
+			if curr&1 == 1 {
+				indegree[requests[pos][0]]--
+				indegree[requests[pos][1]]++
+			}
+			pos--
+		}
+
+		for _, d := range indegree {
+			if d != 0 {
+				continue label
+			}
+		}
+		if bitCount > answer {
+			answer = bitCount
+		}
+	}
+	return answer
+}
