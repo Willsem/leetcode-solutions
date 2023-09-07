cbd65f4858a261d2b6c25ff65039d4e9a5c03195
Willsem <happymainer@gmail.com>
2023-09-07 18:39:03 +0300

    task(92): golang solution

---
+++ solutions/0092. Reverse Linked List II/solution.go
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func reverseBetween(head *ListNode, left int, right int) *ListNode {
+	if head == nil || left == right {
+		return head
+	}
+
+	temp := &ListNode{0, head}
+	prev := temp
+
+	for i := 0; i < left-1; i++ {
+		prev = prev.Next
+	}
+
+	current := prev.Next
+
+	for i := 0; i < right-left; i++ {
+		nextNode := current.Next
+		current.Next = nextNode.Next
+		nextNode.Next = prev.Next
+		prev.Next = nextNode
+	}
+
+	return temp.Next
+}
