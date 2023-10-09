b77a6522aca4d1053b2a707a12a5879c396dcfc8
Willsem <happymainer@gmail.com>
2023-10-09 06:30:14 +0300

    feat(binarySearch): added bool arg for left and right search

--- go-lib/binarySearch.go
+++ go-lib/binarySearch.go
@@ -1,4 +1,4 @@
-func binarySearch(l, r int, comp func(i int) bool) int {
+func binarySearch(l, r int, comp func(i int) bool, leftSearch bool) int {
 	for l < r-1 {
 		mid := (l + r) / 2
 		if comp(mid) {
@@ -6,6 +6,10 @@ func binarySearch(l, r int, comp func(i int) bool) int {
 		} else {
 			r = mid
 		}
+	}
+
+	if leftSearch {
+		return r
 	}
 
 	return l
