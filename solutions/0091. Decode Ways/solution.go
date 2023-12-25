025ba36f07913fb02b874da17c4ef5c0e4a00f22
Alexander Stepanov <happymainer@gmail.com>
2023-12-25 22:21:27 +0300

    task(91): golang solution

---
+++ solutions/0091. Decode Ways/solution.go
@@ -0,0 +1,17 @@
+import "strconv"
+
+func numDecodings(s string) int {
+	dp := make([]int, len(s)+1)
+	dp[len(s)] = 1
+	for i := len(s) - 1; i > -1; i-- {
+		for j := i; j < len(s); j++ {
+			n, _ := strconv.Atoi(s[i : j+1])
+			if 1 <= n && n <= 26 {
+				dp[i] += dp[j+1]
+			} else {
+				break
+			}
+		}
+	}
+	return dp[0]
+}
