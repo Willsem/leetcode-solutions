fcc45891b40a1b47fe046c2d19cbe027af9bf696
Alexander Stepanov <happymainer@gmail.com>
2024-01-05 12:11:46 +0300

    task(300): golang solution

---
+++ solutions/0300. Longest Increasing Subsequence/solution.go
@@ -0,0 +1,29 @@
+import "math"
+
+func lengthOfLIS(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+
+	dp := make([]int, len(nums))
+	for i := range dp {
+		dp[i] = 1
+	}
+
+	for i := 1; i < len(nums); i++ {
+		for j := 0; j < i; j++ {
+			if nums[i] > nums[j] {
+				dp[i] = int(math.Max(float64(dp[i]), float64(dp[j]+1)))
+			}
+		}
+	}
+
+	max := 1
+	for _, v := range dp {
+		if v > max {
+			max = v
+		}
+	}
+
+	return max
+}
