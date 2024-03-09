1c4984a1bb365cc075d61eb829faab4562feaa2c
Alexander Stepanov <happymainer@gmail.com>
2024-03-09 22:48:36 +0300

    task(2540): golang solution

---
+++ solutions/2540. Minimum Common Value/solution.go
@@ -0,0 +1,16 @@
+func getCommon(nums1 []int, nums2 []int) int {
+	i1, i2 := 0, 0
+	for i1 < len(nums1) && i2 < len(nums2) {
+		if nums1[i1] == nums2[i2] {
+			return nums1[i1]
+		}
+
+		if nums1[i1] < nums2[i2] {
+			i1++
+		} else {
+			i2++
+		}
+	}
+
+	return -1
+}
