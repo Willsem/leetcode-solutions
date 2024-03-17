5dca9eca7bebdfbd14c27fc15a386ade8075a044
Alexander Stepanov <happymainer@gmail.com>
2024-03-17 21:39:08 +0300

    task(57): golang solution

---
+++ solutions/0057. Insert Interval/solution.go
@@ -0,0 +1,24 @@
+func insert(intervals [][]int, newInterval []int) [][]int {
+	result := make([][]int, 0)
+
+	i := 0
+	for i < len(intervals) && intervals[i][1] < newInterval[0] {
+		result = append(result, intervals[i])
+		i++
+	}
+
+	for i < len(intervals) && intervals[i][0] <= newInterval[1] {
+		newInterval[0] = min(newInterval[0], intervals[i][0])
+		newInterval[1] = max(newInterval[1], intervals[i][1])
+		i++
+	}
+
+	result = append(result, newInterval)
+
+	for i < len(intervals) {
+		result = append(result, intervals[i])
+		i++
+	}
+
+	return result
+}
