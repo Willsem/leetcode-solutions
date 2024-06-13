9d13f56d6e29e152e2dea471c4ef25d6bd6288e6
Alexander Stepanov <happymainer@gmail.com>
2024-06-13 10:26:52 +0300

    task(2037): golang solution

---
+++ solutions/2037. Minimum Number of Moves to Seat Everyone/solution.go
@@ -0,0 +1,19 @@
+import "sort"
+
+func minMovesToSeat(seats []int, students []int) int {
+	sort.Ints(seats)
+	sort.Ints(students)
+
+	res := 0
+	for i := range seats {
+		res += abs(seats[i] - students[i])
+	}
+	return res
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
