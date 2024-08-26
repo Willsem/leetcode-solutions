78eec2a01bac6a671c3d06333967feb6c31f1de4
Alexander Stepanov <happymainer@gmail.com>
2024-08-26 20:21:00 +0300

    task(590): golang solution

---
+++ solutions/0590. N-ary Tree Postorder Traversal/solution.go
@@ -0,0 +1,26 @@
+/**
+ * Definition for a Node.
+ * type Node struct {
+ *     Val int
+ *     Children []*Node
+ * }
+ */
+func postorder(root *Node) []int {
+	result := make([]int, 0)
+
+	var dfs func(node *Node)
+	dfs = func(node *Node) {
+		if node == nil {
+			return
+		}
+
+		for _, child := range node.Children {
+			dfs(child)
+		}
+
+		result = append(result, node.Val)
+	}
+
+	dfs(root)
+	return result
+}
