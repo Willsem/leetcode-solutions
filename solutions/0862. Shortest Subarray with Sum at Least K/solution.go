8376bd8f3bcea74d4cf7279566f114ed6084c98e
Alexander Stepanov <happymainer@gmail.com>
2024-11-17 20:06:50 +0300

    task(862): golang solution

---
+++ solutions/0862. Shortest Subarray with Sum at Least K/solution.go
@@ -0,0 +1,33 @@
+func shortestSubarray(nums []int, k int) int {
+	n := len(nums)
+	sum := make([]int, n+1)
+
+	for i := 0; i < n; i++ {
+		sum[i+1] = sum[i] + nums[i]
+	}
+
+	q := make([]int, n+1)
+	l, r := 0, 0
+	minLength := n + 1
+
+	for i := 0; i < len(sum); i++ {
+		for r > l && sum[i] >= sum[q[l]]+k {
+			if i-q[l] < minLength {
+				minLength = i - q[l]
+			}
+			l++
+		}
+
+		for r > l && sum[i] <= sum[q[r-1]] {
+			r--
+		}
+
+		q[r] = i
+		r++
+	}
+
+	if minLength <= n {
+		return minLength
+	}
+	return -1
+}
