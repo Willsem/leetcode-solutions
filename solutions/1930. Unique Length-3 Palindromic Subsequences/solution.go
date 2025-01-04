1c3e58cd5dee27b9413d9ad5948d7c05dea18a1f
Alexander Stepanov <happymainer@gmail.com>
2025-01-04 17:40:09 +0300

    task(1930): refactor solution

--- solutions/1930. Unique Length-3 Palindromic Subsequences/solution.go
+++ solutions/1930. Unique Length-3 Palindromic Subsequences/solution.go
@@ -1,23 +1,28 @@
+type orderEl struct {
+	l int
+	r int
+}
+
 func countPalindromicSubsequence(s string) int {
-	order := make([][2]int, 26)
+	order := make([]orderEl, 26)
 	for i := range order {
-		order[i] = [2]int{-1, -1}
+		order[i] = orderEl{-1, -1}
 	}
 
 	for i, ch := range s {
 		idx := ch - 'a'
-		if order[idx][0] != -1 {
-			order[idx][1] = i
+		if order[idx].l != -1 {
+			order[idx].r = i
 		} else {
-			order[idx][0] = i
+			order[idx].l = i
 		}
 	}
 
 	count := 0
 	for _, pos := range order {
-		if pos[1]-pos[0] > 1 {
+		if pos.r-pos.l > 1 {
 			unique := make(map[rune]struct{})
-			for _, midChar := range s[pos[0]+1 : pos[1]] {
+			for _, midChar := range s[pos.l+1 : pos.r] {
 				unique[midChar] = struct{}{}
 			}
 			count += len(unique)
