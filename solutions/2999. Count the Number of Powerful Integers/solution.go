1d183164b26418fd8c9c5386a74ed0d5f179fd47
Alexander Stepanov <happymainer@gmail.com>
2025-04-10 16:50:08 +0300

    task(2999): golang solution

---
+++ solutions/2999. Count the Number of Powerful Integers/solution.go
@@ -0,0 +1,43 @@
+import (
+	"math"
+	"strconv"
+)
+
+func numberOfPowerfulInt(start int64, finish int64, limit int, s string) int64 {
+	startInt := strconv.FormatInt(start-1, 10)
+	finishInt := strconv.FormatInt(finish, 10)
+	return calculate(finishInt, s, limit) - calculate(startInt, s, limit)
+}
+
+func calculate(x string, s string, limit int) int64 {
+	if len(x) < len(s) {
+		return 0
+	}
+
+	if len(x) == len(s) {
+		if x >= s {
+			return 1
+		}
+		return 0
+	}
+
+	suffix := x[len(x)-len(s):]
+	var count int64
+	preLen := len(x) - len(s)
+
+	for i := 0; i < preLen; i++ {
+		digit := int(x[i] - '0')
+
+		if limit < digit {
+			count += int64(math.Pow(float64(limit+1), float64(preLen-i)))
+			return count
+		}
+
+		count += int64(digit) * int64(math.Pow(float64(limit+1), float64(preLen-1-i)))
+	}
+
+	if suffix >= s {
+		count++
+	}
+	return count
+}
