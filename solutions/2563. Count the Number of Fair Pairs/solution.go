982145b43e19d73823b2961447f3166b4d439bd1
Alexander Stepanov <happymainer@gmail.com>
2025-04-19 11:20:05 +0300

    task(2563): format int

--- solutions/2563. Count the Number of Fair Pairs/solution.go
+++ solutions/2563. Count the Number of Fair Pairs/solution.go
@@ -6,17 +6,17 @@ func countFairPairs(nums []int, lower int, upper int) int64 {
 }
 
 func countPairs(nums []int, target int) int64 {
-	var count int64
+	count := 0
 	l, r := 0, len(nums)-1
 
 	for l < r {
 		if nums[l]+nums[r] > target {
 			r--
 		} else {
-			count += int64(r - l)
+			count += r - l
 			l++
 		}
 	}
 
-	return count
+	return int64(count)
 }
