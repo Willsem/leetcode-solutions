ebfa428be9ccf68b29ecae9ceb0c8a137967592a
Alexander Stepanov <happymainer@gmail.com>
2025-06-19 22:39:05 +0300

    task(2294): golang solution

---
+++ solutions/2294. Partition Array Such That Maximum Difference Is K/solution.go
@@ -0,0 +1,16 @@
+import "sort"
+
+func partitionArray(nums []int, k int) int {
+	sort.Ints(nums)
+
+	ans := 1
+	rec := nums[0]
+	for _, num := range nums {
+		if num-rec > k {
+			ans++
+			rec = num
+		}
+	}
+
+	return ans
+}
